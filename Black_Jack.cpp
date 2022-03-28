f4b3ee38d0730ef750b764dbf5b4324da53a42d5
Alexey-Havrilenko <Alexey.Havrilenko@tutamail.com>
2022-03-28 22:38:17 +1030

    initial commit

---
+++ Black_Jack.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+class Player {
+    protected:
+        string name;
+        double bank;
+
+    public:
+        Player() {
+            name = "";
+            bank = 0;
+        }
+};
+
+
+int main()
+{
+    return 0;
+}
