d38531015151609876468c9f64f77c01de4e42b9
Alexey-Havrilenko <Alexey.Havrilenko@tutamail.com>
2022-03-28 23:41:25 +1030

    started the card and player classes

--- Black_Jack.cpp
+++ Black_Jack.cpp
@@ -4,20 +4,93 @@
 
 using namespace std;
 
+class Card {
+    protected:
+        int value;
+        bool isAce;
+        string suit;
+        string name;
+
+
+    public:
+        Card() {
+            value = 0;
+            isAce = false;
+            suit = "";
+            name = "";
+        }
+
+        Card(int cardValue, string cardName, string cardSuit) {
+            value = cardValue;
+            name = cardName;
+            suit = cardSuit;
+            isAce = false;
+
+            if (name == "Ace") {
+                isAce = true;
+            }
+
+        }
+
+        int getValue() {
+            return value;
+        }
+
+        void printCard() {
+            cout << name << " of " << suit << " with a value of " << value << endl;
+        }
+
+};
+
 class Player {
     protected:
         string name;
         double bank;
 
     public:
-        Player() {
-            name = "";
-            bank = 0;
+        Player(string playerName, int playerBank) {
+            name = playerName;
+            bank = playerBank;
+        }
+
+        void setName(string playerName) {
+            name = playerName;
+        }
+
+        string getName() {
+            return name;
+        }
+
+        void setBank(int playerBank) {
+            bank = playerBank;
+        }
+
+        void decreaseBank(int loss) {
+            bank -= loss;
+        }
+
+        void increaseBank(int win) {
+            bank += win;
         }
 };
 
 
 int main()
 {
+    Card deck[52];
+    string suits[] = {"Hearts", "Diamonds", "Spades", "Clubs"};
+    string name[] = {"Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"};
+    int values[] = {2,3,4,5,6,7,8,9,10,10,10,10,11};
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 13; j++) {
+            deck[i] = Card(values[j], name[j], suits[i]);
+            deck[i].printCard();
+        }
+    }
+
+    Player("Player1", 100);
+    Player("Dealer", 500);
+
     return 0;
 }
