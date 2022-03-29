ddcb92f83cff0dd8f7de2655cc709c88b1bb2988
Alexey-Havrilenko <Alexey.Havrilenko@tutamail.com>
2022-03-29 22:35:50 +1030

    fixed the deck generation, changed protected to private (accidentally did this earlier on), further developed functions for the player class and added some more test code in the main loop.

--- Black_Jack.cpp
+++ Black_Jack.cpp
@@ -1,35 +1,33 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class Card {
-    protected:
+    private:
         int value;
-        bool isAce;
+        bool ace;
         string suit;
         string name;
-
-
     public:
         Card() {
             value = 0;
-            isAce = false;
+            ace = false;
             suit = "";
             name = "";
         }
 
-        Card(int cardValue, string cardName, string cardSuit) {
+        Card(string cardName, string cardSuit, int cardValue) {
             value = cardValue;
             name = cardName;
             suit = cardSuit;
-            isAce = false;
+            ace = false;
 
             if (name == "Ace") {
-                isAce = true;
+                ace = true;
             }
-
         }
 
         int getValue() {
@@ -40,15 +38,19 @@ class Card {
             cout << name << " of " << suit << " with a value of " << value << endl;
         }
 
+        bool isAce() {
+            return ace;
+        }
 };
 
 class Player {
-    protected:
+    private:
         string name;
         double bank;
+        vector<Card> cards;
 
     public:
-        Player(string playerName, int playerBank) {
+        Player(string playerName, double playerBank) {
             name = playerName;
             bank = playerBank;
         }
@@ -61,36 +63,93 @@ class Player {
             return name;
         }
 
-        void setBank(int playerBank) {
+        void setBank(double playerBank) {
             bank = playerBank;
         }
 
-        void decreaseBank(int loss) {
-            bank -= loss;
+        void decreaseBank(double bankDelta) {
+            bank -= bankDelta;
         }
 
-        void increaseBank(int win) {
-            bank += win;
+        void increaseBank(double bankDelta) {
+            bank += bankDelta;
+        }
+
+        int placeBet (double betAmount) {
+            if (betAmount >= bank) {
+                bank -= betAmount;
+                return 1;
+            }
+            else {
+                return -1;
+            }
+        }
+
+        int amountOfCards() {
+            return cards.size();
+        }
+
+        void addCard (Card newCard) {
+            cards.push_back(newCard);
+        }
+
+        Card subtractCard () {
+            Card returnCard = cards[-1];
+            cards.pop_back();
+            return returnCard;
+        }
+
+        int playerCardValues () {
+            int cardValues = 0;
+            int amountOfAces = 0;
+
+            for (int i = 0; i < cards.size(); i++) {
+                cardValues += cards[i].getValue(); 
+                if (cards[i].isAce()) {
+                    amountOfAces += 1;
+                }
+            }
+
+            while (cardValues > 21 && amountOfAces > 0) {
+                cardValues -= 10;
+                amountOfAces -= 1;
+            }
+
+            return cardValues;    
+        }
+
+        void printCards() {
+            cout << name << " has the following cards: " << endl;
+            for (int i = 0; i < cards.size(); i++) {
+                cards[i].printCard();
+            }
         }
 };
 
 
 int main()
 {
-    Card deck[52];
-    string suits[] = {"Hearts", "Diamonds", "Spades", "Clubs"};
     string name[] = {"Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"};
+    string suits[] = {"Hearts", "Diamonds", "Spades", "Clubs"};
     int values[] = {2,3,4,5,6,7,8,9,10,10,10,10,11};
 
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 13; j++) {
-            deck[i] = Card(values[j], name[j], suits[i]);
-            deck[i].printCard();
-        }
+    Card deck[52];
+
+    for (int i = 0; i < 52; i++) {
+        deck[i] = Card(name[i/4], suits[i%4], values[i/4]);
+        deck[i].printCard();
     }
 
-    Player("Player1", 100);
-    Player("Dealer", 500);
+    double pot;
 
+    Player player("Player1", 100);
+    Player dealer("Dealer", 500);
+
+    cout << "Player has " << player.amountOfCards() << " cards" << endl;
+    player.addCard(deck[0]);
+    player.addCard(deck[50]);
+    player.addCard(deck[51]);
+    player.printCards();
+    cout << "Player is at " <<  player.playerCardValues() << endl;
     return 0;
 }
